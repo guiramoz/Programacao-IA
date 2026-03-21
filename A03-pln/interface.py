7bfcd0a7984353841602c4563ebd34b6d17e6afd
guiramoz <guilhermeramosap@gmail.com>
2026-03-21 16:54:29 -0300

    Começando o primeiro pln
    Na aula 03 foi desenvolvido os conceitos de neurônios artificiais e pln, com o desenvolvimento de um projeto sensacional !

---
+++ A03-pln/interface.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+import joblib
+import spacy
+import pandas as pd
+
+#configuração de página (título e icone)
+st.set_page_config(page_title="Triagem de chamados",page_icon="👨🏽‍💻")
